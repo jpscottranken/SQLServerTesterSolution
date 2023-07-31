202dfb0c4d2d1fa306a52957a6b436a1a24a546a
Jeff Scott <jpscott@ranken.edu>
2023-07-31 11:47:15 -0500

    SQL Server Query Tester Done In Class M 7/31

---
+++ SQLServerTesterApp/frmQueryTester.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SQLServerTesterApp
+{
+    partial class frmQueryTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMMABooks = new System.Windows.Forms.DataGridView();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblNumberOfRecords = new System.Windows.Forms.Label();
+            this.lblRecordsSelected = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMMABooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMMABooks
+            // 
+            this.dgvMMABooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMMABooks.Location = new System.Drawing.Point(13, 13);
+            this.dgvMMABooks.Name = "dgvMMABooks";
+            this.dgvMMABooks.Size = new System.Drawing.Size(1576, 586);
+            this.dgvMMABooks.TabIndex = 4;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuery.Location = new System.Drawing.Point(24, 619);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(972, 148);
+            this.txtQuery.TabIndex = 0;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.Location = new System.Drawing.Point(1019, 619);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(152, 33);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "&Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(1019, 673);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(152, 33);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(1019, 734);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(152, 33);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblNumberOfRecords
+            // 
+            this.lblNumberOfRecords.BackColor = System.Drawing.Color.Yellow;
+            this.lblNumberOfRecords.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberOfRecords.Location = new System.Drawing.Point(1205, 619);
+            this.lblNumberOfRecords.Name = "lblNumberOfRecords";
+            this.lblNumberOfRecords.Size = new System.Drawing.Size(100, 57);
+            this.lblNumberOfRecords.TabIndex = 5;
+            this.lblNumberOfRecords.Text = "0";
+            this.lblNumberOfRecords.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRecordsSelected
+            // 
+            this.lblRecordsSelected.BackColor = System.Drawing.Color.Yellow;
+            this.lblRecordsSelected.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsSelected.Location = new System.Drawing.Point(1328, 619);
+            this.lblRecordsSelected.Name = "lblRecordsSelected";
+            this.lblRecordsSelected.Size = new System.Drawing.Size(261, 57);
+            this.lblRecordsSelected.TabIndex = 6;
+            this.lblRecordsSelected.Text = "Records Selected";
+            this.lblRecordsSelected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmQueryTester
+            // 
+            this.AcceptButton = this.btnExecute;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(1601, 779);
+            this.Controls.Add(this.lblRecordsSelected);
+            this.Controls.Add(this.lblNumberOfRecords);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.dgvMMABooks);
+            this.Name = "frmQueryTester";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Server Database - Query Tester App";
+            this.Load += new System.EventHandler(this.frmQueryTester_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMMABooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMMABooks;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblNumberOfRecords;
+        private System.Windows.Forms.Label lblRecordsSelected;
+    }
+}
+
---
+++ SQLServerTesterApp/frmQueryTester.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLServerTesterApp
+{
+    public partial class frmQueryTester : Form
+    {
+        public frmQueryTester()
+        {
+            InitializeComponent();
+        }
+
+        //  Global database variables
+
+        //  Taken from URL:
+        //  https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlconnection?view=dotnet-plat-ext-6.0
+        //  A SqlConnection object represents a unique session
+        //  to a SQL Server data source. With a client/server
+        //  database system, it is equivalent to a network
+        //  connection to the server. SqlConnection is used
+        //  together with SqlDataAdapter and SqlCommand to
+        //  increase performance when connecting to a Microsoft
+        //  SQL Server database. For all third-party SQL
+        //  Server products and other OLE DB-supported data
+        //  sources, use OleDbConnection.
+        SqlConnection conn;
+
+        //  Taken from URL:
+        //  https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand?view=dotnet-plat-ext-6.0
+        //  When an instance of SqlCommand is created, the
+        //  read/write properties are set to their initial values. 
+        SqlCommand queryCommand;
+
+        //  Taken from the following URL:
+        //  https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldataadapter?view=dotnet-plat-ext-6.0
+        //  The SqlDataAdapter, serves as a bridge between a
+        //  DataSet and SQL Server for retrieving and saving data.
+        //
+        //  The SqlDataAdapter provides this bridge by mapping Fill,
+        //  which changes the data in the DataSet to match the data
+        //  in the data source, and Update, which changes the data
+        //  in the data source to match the data in the DataSet,
+        //  using the appropriate Transact-SQL statements against
+        //  the data source.
+        SqlDataAdapter queryAdapter;
+
+        //  Taken from the following URL:
+        //  https://www.c-sharpcorner.com/UploadFile/mahesh/datatable-in-C-Sharp/
+        //
+        //  The DataTable class in C# ADO.NET is a database table
+        //  representation and provides a collection of columns and
+        //  rows to store data in a grid form. 
+        DataTable table;
+
+        private void frmQueryTester_Load(object sender, EventArgs e)
+        {
+            //  Set the connection string
+            var connString = @"Data Source =(LocalDb)\MSSQLLocalDB;Initial Catalog=MMABooks; Integrated Security=SSPI";
+
+            //  Create SqlConnection based on connString above
+            conn = new SqlConnection(connString);
+
+            //  Open the connection
+            conn.Open();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            AttemptToExecuteQuery();
+        }
+
+        private void AttemptToExecuteQuery()
+        {
+            //  "Wipe out" whatever is currently
+            //  in the queryCommand.
+            queryCommand = null;
+
+            //  Instantiate new SqlDataAdapter()
+            queryAdapter = new SqlDataAdapter();
+
+            //  Instantiate new DataTable()
+            table = new DataTable();
+
+            //  Check for an empty query input textbox
+            if (txtQuery.Text.Trim() == "")
+            {
+                ShowMessage("Nothing Entered Into Query TextBox",
+                            "QUERY TEXTBOX EMPTY");
+                txtQuery.Focus();
+                return;
+            }
+
+            //  There was something inputted into
+            //  the query textbox. Handle it.
+            try
+            {
+                //  Instantiate a new SqlCommand object
+                queryCommand = new SqlCommand(txtQuery.Text, conn);
+
+                //  Use the queryAdapter as a "bridge" between
+                //  the program and the MMABooks database.
+                queryAdapter.SelectCommand = queryCommand;
+
+                //  Fill the database with the
+                //  output of the queryAdapter.
+                queryAdapter.Fill(table);
+
+                //  Set the data source for
+                //  the dgvMMABooks datagridview.
+                dgvMMABooks.DataSource = table;
+
+                //  Set the number of records
+                //  returned in associated label.
+                lblNumberOfRecords.Text =
+                            table.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Invalid Query In TextBox\n\n" +
+                            ex.Message,
+                            "QUERY TEXTBOX CONTENTS INVALID");
+                txtQuery.Text = "";
+                txtQuery.Focus(); 
+                return;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearTheGUI();
+        }
+
+        private void ClearTheGUI()
+        {
+            //  https://www.codeproject.com/Questions/332902/how-to-clear-datagridview-in-csharp
+            if (this.dgvMMABooks.DataSource != null)
+            {
+                this.dgvMMABooks.DataSource = null;
+            }
+            else
+            {
+                this.dgvMMABooks.Rows.Clear();
+            }
+
+            lblNumberOfRecords.Text = "0";
+            txtQuery.Text = "";
+            txtQuery.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            ExitProgramOrNot();
+        }
+
+        private void ExitProgramOrNot()
+        {
+            DialogResult dialog = MessageBox.Show(
+                "Do You Really Want To Exit?",
+                "EXIT NOW?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                //  Close connection
+                conn.Close();
+                Application.Exit();
+            }
+        }
+
+        private void ShowMessage(string msg, string title)
+        {
+            MessageBox.Show(msg, title,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+        }
+    }
+}
